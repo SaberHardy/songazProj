b0d72d68270edde04b8d5ba350fb0b495b2a7c17
SaberHardy <samirsamado5@gmail.com>
2024-03-20 20:48:37 -0400

    add unique id to the model

--- appSongaz/models.py
+++ appSongaz/models.py
@@ -1,15 +1,21 @@
-from django.contrib.auth.models import User
-from django.db import models
+import uuid
 import os
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+
+
+def generate_unique_id():
+    return str(uuid.uuid4())[:4]
 
 
 class File(models.Model):
     title = models.CharField(max_length=100)
     file = models.FileField(upload_to='files/')
     uploaded_at = models.DateTimeField(auto_now=True)
-    # Link the file with the user
     user = models.ForeignKey(User, related_name="files", on_delete=models.CASCADE)
     arrived = models.BooleanField(default=True)
+    unique_id = models.CharField(max_length=4, unique=True, default=generate_unique_id)
 
     def __str__(self):
         return os.path.basename(self.file.name)
